402a672168f3b7db591dcff41919172b89a5e458
Raito Bezarius <masterancpp@gmail.com>
2023-02-10 19:20:36 +0100

    linux(vf2): so close

--- pkgs/linux-visionfive2/default.nix
+++ pkgs/linux-visionfive2/default.nix
@@ -6,16 +6,16 @@ buildLinux (args // {
   src = fetchFromGitHub {
     owner = "starfive-tech";
     repo = "linux";
-    rev = "JH7110_VisionFive2_upstream";
+    rev = "JH7110_VisionFive2_devel";
     # rev = "JH7110_VisionFive2_upstream";
-    sha256 = "sha256-yjIgLlsuYjOPv52m7pftkeGkiwFAFugFgzFvWK3ZBpU=";
-    # sha256 = "sha256-o1k1UDUXUsRb4200zZ5ozVL15g7wsheet8O5UhX2HWY=";
+    # sha256 = "sha256-yjIgLlsuYjOPv52m7pftkeGkiwFAFugFgzFvWK3ZBpU=";
+    sha256 = "sha256-o1k1UDUXUsRb4200zZ5ozVL15g7wsheet8O5UhX2HWY=";
     # sha256 = "sha256-yjIgLlsuYjOPv52m7pftkeGkiwFAFugFgzFvWK3ZBpU=";
   };
 
   kernelPatches = [
     { name = "crypto-dh"; patch = ./0001-security-keys-dh-dh_data_from_key-takes-const-void-d.patch; }
-    { name = "remove-ciphers"; patch = ./0002-crypto-remove-rmd128-as-it-is-not-here-anymore.patch; }
+    { name = "remove-ciphers"; patch = ./0002-crypto-remove-old-ciphers-as-it-is-not-here-anymore.patch; }
     { name = "sound-soc-starfive"; patch = ./0003-sound-soc-starfive-remove-starfive_pwmdac_transmitte.patch; }
     { name = "gpu-drm-tda998x"; patch = ./0004-drivers-gpu-drm-i2c-tda998x_pin-use-proper-device-ta.patch; }
     { name = "verisilicon"; patch = ./0005-verisilicon-deal-with-it.patch; }
@@ -28,12 +28,17 @@ buildLinux (args // {
 
   structuredExtraConfig = with lib.kernel; {
     # TODO(hacks): cleanup those.
+    DEBUG_INFO_BTF = lib.mkForce no;
+    DEBUG_INFO_BTF_MODULES = lib.mkForce no;
     SND_SOC_WM8960 = no;
-    CRYPTO_SM4 = no;
+    RTC_DRV_STARFIVE = no;
+    PL330_DMA = no;
     SPI_PL022 = no;
     SPI_PL022_STARFIVE = no;
     DRM_VERISILICON = no;
     STARFIVE_INNO_HDMI = no;
+    DRM_IMG_ROGUE = no;
+    USB_WIFI_ECR6600U = no;
     # Required extra configuration
     SOC_STARFIVE = yes;
     CLK_STARFIVE_JH7110_SYS = yes;
